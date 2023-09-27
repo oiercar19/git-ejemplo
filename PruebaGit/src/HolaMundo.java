ec2ae226cb33a60dc86cc5117f38eebeb808d96f
Oier Carbón <145966871+oiercar19@users.noreply.github.com>
2023-09-27 10:46:30 +0200

    Mensaje traducido al ingles

--- PruebaGit/src/HolaMundo.java
+++ PruebaGit/src/HolaMundo.java
@@ -3,7 +3,7 @@ public class HolaMundo {
 	
 	public static void main(String[] args) {
 		int numero = 0;
-		System.out.println("Hola Mundo!!" + numero);
+		System.out.println("Hello World!!" + numero);
 		
 	}
 
